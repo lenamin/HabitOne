4a1110bccc7c3b17a1d76677aa7e8fb0cf180203
Lena <99120199+lenamin@users.noreply.github.com>
2022-05-05 14:13:41 +0900

    Initial Commit

---
+++ MyHabits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyHabits
+//
+//  Created by Lena on 2022/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyHabits/MyHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitsApp.swift
+//  MyHabits
+//
+//  Created by Lena on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
